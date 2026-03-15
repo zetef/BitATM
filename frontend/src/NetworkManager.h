568da228cae985dd63a32bd3a96726b2cac4daf7
Ștefan-Alexandru Dediu <stefan-alexandru.dediu@stud.mta.ro>
2026-03-15 21:16:07 +0200

    feat(fronted): echo text box && live connection

--- frontend/src/NetworkManager.h
+++ frontend/src/NetworkManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QList>
+#include <QNetworkInformation>
 #include <QObject>
 #include <QSslError>
 #include <QString>
@@ -8,15 +9,21 @@
 
 class NetworkManager : public QObject {
     Q_OBJECT
-    Q_PROPERTY(bool isConnected READ isConnected NOTIFY connected)
+    Q_PROPERTY(bool isConnected READ isConnected NOTIFY connectionChanged)
+    Q_PROPERTY(QString lastMessage READ lastMessage NOTIFY lastMessageChanged)
+    Q_PROPERTY(bool hasError READ hasError NOTIFY lastMessageChanged)
 public:
     explicit NetworkManager(QObject* parent = nullptr);
     void connectToServer(const QUrl& url);
-    void sendText(const QString& text);
+    Q_INVOKABLE void sendText(const QString& text);
     bool isConnected() const;
+    QString lastMessage() const;
+    bool hasError() const;
 
 signals:
     void messageReceived(const QString& message);
+    void lastMessageChanged();
+    void connectionChanged();
     void connected();
     void disconnected();
     void errorOcurred(const QString& errorString);
@@ -26,7 +33,12 @@ private slots:
     void onDisconnected();
     void onTextMessageReceived(const QString& message);
     void onSslErrors(const QList<QSslError>& errors);
+    void onReachabilityChanged(QNetworkInformation::Reachability reachability);
 
 private:
     QWebSocket _socket;
+    QUrl _serverUrl;
+    QString _lastMessage;
+    bool _hasError = false;
+    bool _firstConnect = true;
 };
