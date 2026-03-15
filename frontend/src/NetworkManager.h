ff0933d7dbc3be58cf9bb70aa142a80c181f561b
Ștefan-Alexandru Dediu <stefan-alexandru.dediu@stud.mta.ro>
2026-03-15 19:57:14 +0200

    feat(NetworkManager): add isConnected property, disconnected wiring, SSL error handling

--- frontend/src/NetworkManager.h
+++ frontend/src/NetworkManager.h
@@ -1,24 +1,31 @@
 #pragma once
+#include <QList>
 #include <QObject>
+#include <QSslError>
 #include <QString>
 #include <QUrl>
 #include <QWebSocket>
 
 class NetworkManager : public QObject {
     Q_OBJECT
+    Q_PROPERTY(bool isConnected READ isConnected NOTIFY connected)
 public:
     explicit NetworkManager(QObject* parent = nullptr);
     void connectToServer(const QUrl& url);
     void sendText(const QString& text);
+    bool isConnected() const;
 
 signals:
     void messageReceived(const QString& message);
     void connected();
     void disconnected();
+    void errorOcurred(const QString& errorString);
 
 private slots:
     void onConnected();
+    void onDisconnected();
     void onTextMessageReceived(const QString& message);
+    void onSslErrors(const QList<QSslError>& errors);
 
 private:
     QWebSocket _socket;
