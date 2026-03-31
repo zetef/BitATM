f7949a222d383e39c482cfcdd8abe5f9ac3c6e0d
Ștefan-Alexandru Dediu <stefan-alexandru.dediu@stud.mta.ro>
2026-03-31 11:05:17 +0300

    feat(backend): add OfflineMessage domain class

---
+++ backend/src/OfflineMessage.cpp
@@ -0,0 +1,3 @@
+#include "OfflineMessage.h"
+
+OfflineMessage::OfflineMessage() {}
---
+++ backend/src/OfflineMessage.h
@@ -0,0 +1,9 @@
+#ifndef OFFLINEMESSAGE_H
+#define OFFLINEMESSAGE_H
+
+class OfflineMessage {
+public:
+    OfflineMessage();
+};
+
+#endif  // OFFLINEMESSAGE_H
